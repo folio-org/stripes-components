ea90f200fb6f2a63dd9e7eebc97afb1db86ac27c
Mike Taylor <mike@indexdata.com>
2017-03-17 12:44:12 +0000

    Parse query elements out of search-string as needed
    Under react-router v4.0.0-alpha.6, which is what we were using until
    recently, the location object has parsed-out query parameters in the
    `query` element. In v4.0 proper, that is no longer provided. So in the
    absence of a pre-parsed query, we now do it ourselves, using
    queryString.parse(location.search).
    
    Fixes STRIPES-258.

--- util/transitionToParams.js
+++ util/transitionToParams.js
@@ -1,6 +1,12 @@
+import queryString from 'query-string';
+
 function transitionToParams(params) {
   const location = this.props.location;
-  const allParams = Object.assign({}, location.query, params);
+  let query = location.query;
+  if (query == undefined)
+    query = queryString.parse(location.search);
+
+  const allParams = Object.assign({}, query, params);
   const keys = Object.keys(allParams);
 
   let url = location.pathname;
