d70b4d8820046daa13216207b703f769e86c0b36
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-03-21 12:21:18 +0100

    Outcommented ID test for Checkbox since we removed ID's from Checkboxes for now.

--- test/Checkbox/Checkbox.test.js
+++ test/Checkbox/Checkbox.test.js
@@ -9,31 +9,31 @@ describe('Checkbox', () => {
   /**
    * Ensure that Checkbox always renders with a unique ID
    */
-  it('should always render with a unique ID ', (done) => {
-    const items = [];
-    const found = [];
-    const duplicates = [];
-    for (let i = 0; i < 100; i++) {
-      const myCustomId = i === 50 ? 'my-custom-id' : null;
-      items.push(<Checkbox className="checkbox" id={myCustomId} key={i} />);
-    }
-    const wrapper = shallow(<div>{items}</div>);
-    wrapper.find('.checkbox').forEach((node) => {
-      const id = node.render().find('input')[0].attribs.id;
-      if (found.indexOf(id) >= 0) {
-        duplicates.push(id);
-      }
-      found.push(id);
-    });
-
-    // Check for duplicates
-    expect(duplicates.length, 'duplicates').to.equal(0);
-
-    // Check if custom ID was added
-    expect(found, 'Custom ID is not added to Checkbox').to.include.members(['my-custom-id']);
-
-    done();
-  });
+  // it('should always render with a unique ID ', (done) => {
+  //   const items = [];
+  //   const found = [];
+  //   const duplicates = [];
+  //   for (let i = 0; i < 100; i++) {
+  //     const myCustomId = i === 50 ? 'my-custom-id' : null;
+  //     items.push(<Checkbox className="checkbox" id={myCustomId} key={i} />);
+  //   }
+  //   const wrapper = shallow(<div>{items}</div>);
+  //   wrapper.find('.checkbox').forEach((node) => {
+  //     const id = node.render().find('input')[0].attribs.id;
+  //     if (found.indexOf(id) >= 0) {
+  //       duplicates.push(id);
+  //     }
+  //     found.push(id);
+  //   });
+  //
+  //   // Check for duplicates
+  //   expect(duplicates.length, 'duplicates').to.equal(0);
+  //
+  //   // Check if custom ID was added
+  //   expect(found, 'Custom ID is not added to Checkbox').to.include.members(['my-custom-id']);
+  //
+  //   done();
+  // });
 
   /**
    * Test for redux-form compabillity
