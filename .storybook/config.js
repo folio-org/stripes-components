273eff526bd7c26b4f63cbe65236c2a2631ddff8
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-05-21 13:10:01 +0800

    Added mock context decorator to the storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import '../lib/global.css';
+
+/**
+ * Add mock context
+ */
+import withMockContext from './util/withMockContext';
+addDecorator(withMockContext);
 
 setOptions({
   name: 'FOLIO Stripes',
