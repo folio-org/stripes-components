09c7000d9e923271f620cc9b72ba90306827834a
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-05-25 16:14:44 +0200

    Switched out mock context for storybook-addon-intl.

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,12 +2,35 @@ import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import '../lib/global.css';
 
-/**
- * Add mock context
- */
-import withMockContext from './util/withMockContext';
-addDecorator(withMockContext);
 
+/**
+ * React intl support
+ */
+
+// Load the locale data for all your defined locales
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
+import enTranslations from '../translations/en.json';
+import { addLocaleData } from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
+addLocaleData(enLocaleData);
+
+// Define messages
+const messages = {
+  en: enTranslations,
+};
+
+// Set intl configuration
+setIntlConfig({
+    locales: ['en'],
+    defaultLocale: 'en',
+    getMessages: (locale) => messages[locale]
+});
+
+addDecorator(withIntl);
+
+/**
+ * Set options
+ */
 setOptions({
   name: 'FOLIO Stripes',
 });
