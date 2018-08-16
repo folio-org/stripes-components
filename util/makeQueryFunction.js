folio-org/stripes-components

commit 1bf498d38ba5d625981b02ca764d4b1864008836

Include util directory in linting

util/makeQueryFunction.js
@@ -1,5 +1,5 @@
-import { filters2cql } from '../lib/FilterGroups';
 import { compilePathTemplate } from '@folio/stripes-connect/RESTResource/RESTResource';
+import { filters2cql } from '../lib/FilterGroups';
 
 // failOnCondition can take values:
 //      0: do not fail even if query and filters and empty
@@ -10,7 +10,6 @@ import { compilePathTemplate } from '@folio/stripes-connect/RESTResource/RESTRes
 //
 function makeQueryFunction(findAll, queryTemplate, sortMap, filterConfig, failOnCondition) {
   return (queryParams, pathComponents, resourceValues, logger) => {
-
     const { qindex, filters, query, sort } = resourceValues.query || {};
 
     if ((query === undefined || query === '') &&
@@ -23,19 +22,19 @@ function makeQueryFunction(findAll, queryTemplate, sortMap, filterConfig, failOn
       return null;
     }
 
-    //This check should remain in place until all uses of the $QUERY syntax have been removed from stripes modules
-    if (queryTemplate.includes("$QUERY")) {
-      console.warn('Use of "$QUERY" in the queryTemplate is deprecated. Use the "%{query.query}" syntax instead, as found at https://github.com/folio-org/stripes-connect/blob/master/doc/api.md#text-substitution')
-      queryTemplate = queryTemplate.replace(/\$QUERY/g, '?{query}');
+    // This check should remain '$QUERY' until all uses of the $QUERY syntax have been removed from stripes modules
+    if (queryTemplate.includes('$QUERY')) {
+      console.warn('Use of "$QUERY" in the queryTemplate is deprecated. Use the "%{query.query}" syntax instead, as found at https://github.com/folio-org/stripes-connect/blob/master/doc/api.md#text-substitution');
+      queryTemplate = queryTemplate.replace(/\$QUERY/g, '?{query}'); // eslint-disable-line no-param-reassign
     }
 
-    let cql = undefined;
+    let cql;
     if (query && qindex) {
       const t = qindex.split('/', 2);
       if (t.length === 1) {
         cql = `${qindex}="${query}*"`;
       } else {
-        cql = `${t[0]} =\/${t[1]} "${query}*"`;
+        cql = `${t[0]} =\${t[1]} "${query}*"`;
       }
     } else if (query) {
       cql = compilePathTemplate(queryTemplate, queryParams, pathComponents, resourceValues);
@@ -58,7 +57,7 @@ function makeQueryFunction(findAll, queryTemplate, sortMap, filterConfig, failOn
       const sortIndexes = sort.split(',').map((sort1) => {
         let reverse = false;
         if (sort1.startsWith('-')) {
-          sort1 = sort1.substr(1);
+          sort1 = sort1.substr(1); // eslint-disable-line no-param-reassign
           reverse = true;
         }
         let sortIndex = sortMap[sort1] || sort1;

util/craftLayerUrl.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import queryString from 'query-string';
 
 function craftLayerUrl(mode) {
   const url = this.props.location.pathname + this.props.location.search;

util/detectElementResize.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 /**
  * Detect Element Resize.
  * https://github.com/sdecima/javascript-detect-element-resize

util/getNextFocusable.js
@@ -5,13 +5,14 @@
 */
 
 import contains from 'dom-helpers/query/contains';
-export default function getNextFocusable(currentElement, containing = true) {
-  //add all elements we want to include in our selection
+
+export default function getNextFocusable(currentElement, containing = true) { // eslint-disable-line consistent-return
+  // add all elements we want to include in our selection
   const focusableElements = 'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
   let focusable = Array.prototype.filter.call(document.querySelectorAll(focusableElements),
-    function (element) {
-      //check for visibility while always include the current activeElement
-      return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+    (element) => {
+      // check for visibility while always include the current activeElement
+      return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement;
     });
   if (!containing) {
     const outside = Array.prototype.filter.call(focusable, (element) => {

util/transitionToParams.js
@@ -3,8 +3,9 @@ import queryString from 'query-string';
 function transitionToParams(params) {
   const location = this.props.location;
   let query = location.query;
-  if (query === undefined)
+  if (query === undefined) {
     query = queryString.parse(location.search);
+  }
 
   const allParams = Object.assign({}, query, params);
   const keys = Object.keys(allParams);

commit 1208be00329811c5ac2919e6f14bd746ca0a0060

Correct handling of relation modifiers
Fixes a regression from 1bf498d38ba5d625981b02ca764d4b1864008836

Fixes STCOM-321.

util/makeQueryFunction.js
@@ -38,7 +38,7 @@ function makeQueryFunction(findAll, queryTemplate, sortMap, filterConfig, failOn
       if (t.length === 1) {
         cql = `${qindex}="${query}*"`;
       } else {
-        cql = `${t[0]} =\${t[1]} "${query}*"`;
+        cql = `${t[0]} =\/${t[1]} "${query}*"`;
       }
     } else if (query) {
       cql = compilePathTemplate(queryTemplate, nsQueryParams, pathComponents, { query: resourceQuery });