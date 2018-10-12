ff97d2506a23c8d0e4c4f8d765ca85264d0312aa
John Coburn <jcoburn@ebsco.com>
2018-10-12 12:28:45 -0500

    consider Layers as an exclusion

--- util/trapFocus.js
+++ util/trapFocus.js
@@ -1,14 +1,18 @@
 import contains from 'dom-helpers/query/contains';
+import layerCss from '../lib/Layer/Layer.css';
 
 const getDefaultExceptions = () => [
   document.getElementById('OverlayContainer'),
   ...document.querySelectorAll('.tether-element'),
+  ...document.querySelectorAll(`.${layerCss.LayerRoot}`)
 ];
 
 export default function trapFocus(container, exceptions = getDefaultExceptions()) {
-  if (container && !contains(container, document.activeElement)) {
-    if (exceptions.filter((e) => contains(e, document.activeElement)).length === 0) {
-      container.focus();
+  if (container && container.className !== document.activeElement.className) {
+    if (container && !contains(container, document.activeElement)) {
+      if (exceptions.filter((e) => contains(e, document.activeElement)).length === 0) {
+        container.focus();
+      }
     }
   }
 }
