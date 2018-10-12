ca75a87ed340018eb361de853dc958bc21f1fb52
John Coburn <jcoburn@ebsco.com>
2018-10-12 15:02:02 -0500

    clean-up Layer-specific code

--- util/trapFocus.js
+++ util/trapFocus.js
@@ -1,16 +1,14 @@
 import contains from 'dom-helpers/query/contains';
-import layerCss from '../lib/Layer/Layer.css';
 
 const getDefaultExceptions = () => [
   document.getElementById('OverlayContainer'),
-  ...document.querySelectorAll('.tether-element'),
-  ...document.querySelectorAll(`.${layerCss.LayerRoot}`)
+  ...document.querySelectorAll('.tether-element')
 ];
 
-export default function trapFocus(container, exceptions = getDefaultExceptions()) {
+export default function trapFocus(container, exceptions) {
   if (container && container.className !== document.activeElement.className) {
     if (container && !contains(container, document.activeElement)) {
-      if (exceptions.filter((e) => contains(e, document.activeElement)).length === 0) {
+      if ([...getDefaultExceptions(), ...exceptions].filter((e) => contains(e, document.activeElement)).length === 0) {
         container.focus();
       }
     }
