7436240aa84338722c4f6a0f4ebd93260bdeb3ce
Zak Burke <zburke@cornell.edu>
2019-05-20 11:23:02 -0400

    implement currency select menu
    Refs STSMACOM-205

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ export { default as Badge } from './lib/Badge';
 export { default as Button } from './lib/Button';
 export { default as ButtonGroup } from './lib/ButtonGroup';
 export { default as Checkbox } from './lib/Checkbox';
+export { default as CurrencySelect } from './lib/CurrencySelect';
 export { default as Datepicker, Calendar } from './lib/Datepicker';
 export { default as DateRangeWrapper } from './lib/DateRangeWrapper';
 export { default as EmptyMessage } from './lib/EmptyMessage';
