448768b97443330dad9ca5f2f62dd88c6865fbfe
Rasmus Wølk <640976+rasmuswoelk@users.noreply.github.com>
2019-09-13 17:43:15 +0200

    STCOM-567: Add <Tooltip> component (#1049)
    * Added work from tooltip spike
    
    * Updated popper prop-types and made AVAILABLE_PLACEMENTS exportable
    
    * Some cleanup and comment updates
    
    * Renamed 'label' -> 'text' and 'shortcut' -> 'sub'
    
    * Minor style tweaks and moved the portal definition into componentDidMount
    
    * Fixed some linting errors related to this PR.
    
    * Removed aria-attribute determination and replaced with a more manual approach. Updated the aria validation. Updated examples.
    
    * Updated readme for <Tooltip>
    
    * Added some tests for <Tooltip>

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ export { default as Select } from './lib/Select';
 export { default as TextArea } from './lib/TextArea';
 export { default as TextField } from './lib/TextField';
 export { default as Timepicker } from './lib/Timepicker';
+export { default as Tooltip } from './lib/Tooltip';
 export { default as Editor } from './lib/Editor';
 export { default as MultiSelection } from './lib/MultiSelection';
 export { default as RepeatableField } from './lib/RepeatableField';
