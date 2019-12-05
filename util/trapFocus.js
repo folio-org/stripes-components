188bdad42783468a143c3a9e49a60b6e414c2fee
John Coburn <jcoburn@ebsco.com>
2019-12-05 13:46:16 -0600

    Dropdown hotfixes (#1116)
    * stop propagation on overlay, include header in focus trap exclusion
    
    * remove elipsis
    
    * add comments
    
    * log changes

--- util/trapFocus.js
+++ util/trapFocus.js
@@ -3,12 +3,16 @@ import contains from 'dom-helpers/query/contains';
 const getDefaultExceptions = () => [
   document.getElementById('OverlayContainer'),
   ...document.querySelectorAll('.tether-element'),
-];
+  document.querySelector('header'), // fix for main navigation dropdowns
+];                                  // which depend on having focus when <Layer> is open.
 
 export default function trapFocus(container, exceptions) {
   if (container && container.className !== document.activeElement.className) {
     if (!contains(container, document.activeElement)) {
-      if ([...getDefaultExceptions(), ...exceptions].filter((e) => contains(e, document.activeElement)).length === 0) {
+      if ([...getDefaultExceptions(), ...exceptions].filter((e) => {
+        if (!e) return false;
+        return contains(e, document.activeElement);
+      }).length === 0) {
         container.focus();
       }
     }
