6cd71274df72b3523f4f6ab0dd7e07090c5d0e52
John Coburn <jcoburn@ebsco.com>
2019-12-05 12:45:42 -0600

    remove elipsis

--- util/trapFocus.js
+++ util/trapFocus.js
@@ -3,13 +3,16 @@ import contains from 'dom-helpers/query/contains';
 const getDefaultExceptions = () => [
   document.getElementById('OverlayContainer'),
   ...document.querySelectorAll('.tether-element'),
-  ...document.querySelector('header'),
+  document.querySelector('header'),
 ];
 
 export default function trapFocus(container, exceptions) {
   if (container && container.className !== document.activeElement.className) {
     if (!contains(container, document.activeElement)) {
-      if ([...getDefaultExceptions(), ...exceptions].filter((e) => contains(e, document.activeElement)).length === 0) {
+      if ([...getDefaultExceptions(), ...exceptions].filter((e) => {
+        if (!e) return false;
+        return contains(e, document.activeElement);
+      }).length === 0) {
         container.focus();
       }
     }
