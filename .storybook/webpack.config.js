491a7541f5488a65c7f706dd4dcb9361ad70f0a1
John Coburn <jcoburn@ebsco.com>
2019-12-05 16:20:04 -0600

    implement paging button

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -53,9 +53,6 @@ module.exports = {
         use: [
           {
             loader: 'style-loader',
-            options: {
-              sourceMap: true,
-            },
           },
           {
             loader: 'css-loader',
