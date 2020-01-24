a8b8cd5d5d8d06a60ef289e240f100ab4f002570
Mark Deutsch <mark.deutsch@duke.edu>
2020-01-24 10:52:49 -0500

    Upgrade to Babel 7.8

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -50,13 +50,10 @@ module.exports = {
             ['@babel/plugin-proposal-throw-expressions'],
 
             // Stage 3
-            ['@babel/plugin-syntax-dynamic-import'],
             ['@babel/plugin-syntax-import-meta'],
             ['@babel/plugin-proposal-class-properties', { loose: true }],
-            ['@babel/plugin-proposal-json-strings'],
 
             // Others
-            ['@babel/plugin-proposal-optional-chaining'],
             ['react-hot-loader/babel'],
           ]
         },
