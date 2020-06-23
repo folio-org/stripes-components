ebd88e04964d85fa26157069bf78540ca9f108e4
Rasmus Wølk <rasmuswoelk@gmail.com>
2020-06-23 15:22:58 +0200

    Updated Storybook config files

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-import 'storybook-readme/register';
-import '@storybook/addon-options/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-knobs/register';
-import 'storybook-addon-intl/register';
-import 'storybook-addon-rtl/register';
---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: ['../lib/**/*.stories.[tj]s'],
+  addons: [
+    'storybook-readme/register',
+    '@storybook/addon-options/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    'storybook-addon-intl/register',
+    'storybook-addon-rtl/register',
+  ],
+};
+
+
+
+
+
+
+
+
