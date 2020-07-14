6106f7c6269f9a65e8adb13146285463fd9412b7
Rasmus Wølk <rasmuswoelk@gmail.com>
2020-07-14 13:33:01 +0200

    Updated <Datepicker> to use the custom RootCloseWrapper

--- util/RootCloseWrapper.js
+++ util/RootCloseWrapper.js
@@ -1,7 +1,6 @@
-import React, { useRef, forwardRef } from 'react';
+import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import useRootClose from 'react-overlays/useRootClose';
-import childrenOf from './childrenOf';
 
 const RootCloseWrapper = forwardRef(({ children, onRootClose, disabled }, ref) => {
   useRootClose(ref, onRootClose, {
