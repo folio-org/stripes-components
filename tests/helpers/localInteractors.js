4e668e221e12dfe31eeca4171a68616762f9546e
John Coburn <jcoburn@ebsco.com>
2021-10-15 15:39:33 -0500

    selection interactor/test

--- tests/helpers/localInteractors.js
+++ tests/helpers/localInteractors.js
@@ -3,3 +3,8 @@ import { HTML } from '@folio/stripes-testing';
 
 export const label = HTML.extend('label-interactor')
   .selector('label');
+
+export const RoledHTML = HTML.extend('html plus')
+  .filters({
+    role: element => element.getAttribute('role')
+  });
