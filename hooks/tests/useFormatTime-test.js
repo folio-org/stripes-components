6aa84d95cd44b2fbb57824414dd753e100249421
Zak Burke <zburke@ebsco.com>
2023-06-21 15:01:31 -0400

    Mario say, 'you wanna use the hook, youa gottawait the hook'

--- hooks/tests/useFormatTime-test.js
+++ hooks/tests/useFormatTime-test.js
@@ -7,10 +7,10 @@ import { expect } from 'chai';
 import getHookExecutionResult from '../../tests/helpers/getHookExecutionResult';
 import useFormatTime from '../useFormatTime';
 
-describe('useFormatTime', () => {
-  const hookResult = getHookExecutionResult(useFormatTime);
-
+describe.only('useFormatTime', () => {
   it('should return correct time', () => {
-    expect(hookResult('2020-03-24T17:59:57.369+0000')).to.equal('5:59 PM');
+    getHookExecutionResult(useFormatTime).then(hookResult => {
+      expect(hookResult('2020-03-24T17:59:57.369+0000')).to.equal('5:59 PM');
+    });
   });
 });
