cad0804e5428008f5cc94766b650ae2eaaa0e7de
John Coburn <JCoburn@EBSCO.COM>
2023-06-22 12:23:18 -0500

    use react 18, fix 9/11 tests

--- tests/helpers/getHookExecutionResult.js
+++ tests/helpers/getHookExecutionResult.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { mountWithContext } from '../helpers';
 
+/**
+ * getHookExecutionResult
+ * Test a hook by returning its result in a promise.
+ *
+ * @param {*} hook the hook to test
+ * @param {*} hookArguments arguments to pass to the hook
+ * @returns hook's result, wrapped in a Promise, because that's what mountWithContext
+ *   returns and we need to wait for the component that calls the hook to mount and
+ *   render.
+ */
 const getHookExecutionResult = (hook, hookArguments = []) => {
   let result = {};
   const TestComponent = () => {
@@ -11,12 +21,10 @@ const getHookExecutionResult = (hook, hookArguments = []) => {
       Object.assign(result, hookResult);
     }
 
-    return null;
+    return <></>;
   };
 
-  mountWithContext(<TestComponent />);
-
-  return result;
+  return mountWithContext(<TestComponent />).then(() => result);
 };
 
 export default getHookExecutionResult;
