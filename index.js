c12a52ca92063aab01578c7fa21134a90313b062
John Coburn <jcoburn@ebsco.com>
2024-09-04 15:32:36 -0500

    implement screen-reader trapping

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ export {
   getLastFocusable,
   getFirstFocusable
 } from './util/getFocusableElements';
-
+export * from './util/consts';
 export {
   default as currencies,
   currenciesByCode,
